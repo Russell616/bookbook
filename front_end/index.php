2e74ac2fa7e47dc749703f818924ba9d387fe125
Hugo <hugo.braz@ist.utl.pt>
2015-07-13 23:48:47 +0100

    alterado titulo da página

--- front_end/index.php
+++ front_end/index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Shop Homepage - Start Bootstrap Template</title>
+    <title>Book Book</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
