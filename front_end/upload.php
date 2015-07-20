550f959c544b7f8eaa63fa8fcf5d8cf921587cc8
Hugo <hugo.braz@ist.utl.pt>
2015-07-20 11:55:20 +0100

    novo div para por os elementos mais à direita

--- front_end/upload.php
+++ front_end/upload.php
@@ -58,6 +58,7 @@
     
     <div class="container">
         <div class="row">
+            <div class="col-md-3"></div>            
             <div class="col-md-9">
                 <div class="caption-full">
                     <form action= "index.php" method="post" target="_blank" >
