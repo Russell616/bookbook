91ec3b3bf022c9a01b355bd4a73134e2cfc43b00
Hugo <hugo.braz@ist.utl.pt>
2015-07-22 13:01:55 +0100

    tentativa de por os argumentos POST a funcionar...

--- front_end/index.php
+++ front_end/index.php
@@ -28,6 +28,31 @@
 </head>
 
 <body>
+    <script>
+        function post(path, params, method) {
+        method = method || "post"; // Set method to post by default if not specified.
+    
+        // The rest of this code assumes you are not using a library.
+        // It can be made less wordy if you use one.
+        var form = document.createElement("form");
+        form.setAttribute("method", method);
+        form.setAttribute("action", path);
+    
+        for(var key in params) {
+            if(params.hasOwnProperty(key)) {
+                var hiddenField = document.createElement("input");
+                hiddenField.setAttribute("type", "hidden");
+                hiddenField.setAttribute("name", key);
+                hiddenField.setAttribute("value", params[key]);
+    
+                form.appendChild(hiddenField);
+             }
+        }
+    
+        document.body.appendChild(form);
+        form.submit();
+    }
+    </script>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -154,8 +179,9 @@
                   <img src="http://placehold.it/320x150" alt="">
                   <div class="caption">';
                   
-                  echo '<h4><a onclick="setArgs('.  $row['titulo'] .','. $row['autor'] .')"  href=item.php >'. $row['titulo'] .'</a></h4>';
-		$fp = fopen($row['autor'] . '_' . $row['titulo'] . '_desc.txt', 'r');
+                  setArgs($row['titulo'], $row['autor']);
+                  echo '<h4><a onclick="post("index.php", {t: '. $row['titulo'] .', a: ' . $row['autor'] . '})" href=item.php >'. $row['titulo'] .'</a></h4>';
+		    $fp = fopen($row['autor'] . '_' . $row['titulo'] . '_desc.txt', 'r');
 			if (!$fp) {
    			 echo '<p>DEBUG: vazio...</p>'; //FIXME: APENAS TEMPORARIO!!
 			}
