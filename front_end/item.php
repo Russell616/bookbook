af2935d2e6962668c94035cf01bf6c591d296bd8
Hugo <hugo.braz@ist.utl.pt>
2015-07-22 11:45:01 +0100

    removido o "shop name"

--- front_end/item.php
+++ front_end/item.php
@@ -66,7 +66,6 @@
         <div class="row">
 
             <div class="col-md-3">
-                <p class="lead">Shop Name</p>
                 <div class="list-group">
                     <?php
         			$host="localhost"; // o MySQL esta disponivel nesta maquina
